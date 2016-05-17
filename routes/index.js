1c63575967ef74008c3f9244dc6b6b50038abc50
Ganesh Iyer <lastlegion@gmail.com>
2016-05-17 02:52:30 +0000

    asking user for width and height

--- routes/index.js
+++ routes/index.js
@@ -94,10 +94,11 @@ var fetchMetaData = function fetchMetaData(case_id, callback){
     var bindaas_api_key = config.bindaas_api_key;
 
     var bindaas_metadata_endpoint = config.bindaas_metadata_endpoint;
-    var url = bindaas_host + bindaas_project + bindaas_provider + bindaas_metadata_endpoint + "?api_key="+bindaas_api_key + "&TCGAId="+case_id;
+    var url = bindaas_host + "/services"+ bindaas_project + bindaas_provider + "/query/getMetaDataForCaseID" + "?api_key="+bindaas_api_key + "&TCGAId="+case_id;
+   console.log(url);
     superagent.get(url)
         .end(function(err, res){
-            callback(err,res.body);
+            callback(err,res);
         });    
     
 }
@@ -109,13 +110,16 @@ queue.process("MaskOrder", function(job, done) {
     var filePath=  job.data.maskFilePath;
     var OPENCV_DIR = config.OPENCV_DIR ;
     var MONGODB_LOADER_PATH = config.MONGODB_LOADER_PATH;
-
+    /*
     fetchMetaData(case_id, function(err, metadata){
+	console.log(err)
+	console.log(metadata);
         console.log(metadata.body);
         console.log(metadata);
-        var width = metadata[0].width;
-        var height = metadata[0].height;
-        var norm = width +","+height;
+        //var width = metadata[1].width;
+        //var height = metadata[0].height;
+    */
+        var norm = job.data.width +","+job.data.height;
         var conversion_command = "java -Djava.library.path=" + OPENCV_DIR + " -jar " + MONGODB_LOADER_PATH + " --inptype maskfile --inpfile " + filePath + " --dest file --outfolder temp/ --eid " + execution_id + " --etype challenge --cid " + case_id + " --norm "+norm ;
         winston.log("info", "Executing: " + conversion_command);
         try {
@@ -138,6 +142,7 @@ queue.process("MaskOrder", function(job, done) {
                     async.map(payLoads, function(payLoad, cb){
                         postMarkupToBindaas(payLoad, function(err, post_response){
                             if(err.statusCode != 200){
+				console.log(err);
                                 done(err); //Send error to Kue
                                 return; 
                             } else {
@@ -161,7 +166,7 @@ queue.process("MaskOrder", function(job, done) {
             return;
         }
         
-    });
+    //});
 });
 
 
@@ -174,13 +179,16 @@ router.post('/submitMaskOrder', upload.single('mask'), function(req, res, next){
     var maskFile = req.file;
     var case_id = req.body.case_id;
     var execution_id = req.body.execution_id;
-    if(maskFile && case_id && execution_id) {
+    var width = req.body.width;
+    var height = req.body.height;
+    if(maskFile && case_id && execution_id && width && height) {
         var filePath = maskFile.path;
         var job = queue.create("MaskOrder", {
             maskFilePath: filePath,
             case_id: case_id,
             title: "Case_id: "+case_id + " Execution_id: "+execution_id,
-            execution_id: execution_id
+            execution_id: execution_id,
+            width: width, height: height
         }).save(function(err){
             if(!err){
 
@@ -197,9 +205,9 @@ router.post('/submitMaskOrder', upload.single('mask'), function(req, res, next){
         var error_str = "";
         if(!maskFile)
             error_str += " maskfile ";
-        if(!imageId)
+        if(!case_id)
             error_str += " imageId ";
-        if(!userId)
+        if(!execution_id)
             error_str += " userId ";
         return res.status(400).send("Couldn't find :"+error_str);
     }
